9263667730e0cf66673d2520c8dc47bf1c4c99dc
Srithep Witayapanpracha <srithep.witayapanpracha@scbtechx.io>
2025-02-07 17:16:34 +0700

    Add floating move action

--- app/src/main/kotlin/com/peachgenz/currentactivity/FloatingWindow.kt
+++ app/src/main/kotlin/com/peachgenz/currentactivity/FloatingWindow.kt
@@ -1,14 +1,22 @@
 package com.peachgenz.currentactivity
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.PixelFormat
 import android.view.Gravity
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.ViewGroup
 import android.view.WindowManager
 import com.peachgenz.currentactivity.databinding.LayoutFloatingWindowBinding
 
+
 class FloatingWindow(private val mContext: Context) {
+
+    private var xInitCord: Int = 0
+    private var yInitCord: Int = 0
+    private var xInitMargin: Int = 0
+    private var yInitMargin: Int = 0
 
     private val mWindowManager: WindowManager by lazy {
         (mContext.getSystemService(Context.WINDOW_SERVICE) as WindowManager)
@@ -19,24 +27,64 @@ class FloatingWindow(private val mContext: Context) {
     }
 
     private val mBinding: LayoutFloatingWindowBinding by lazy {
-        LayoutFloatingWindowBinding.inflate(LayoutInflater.from(mContext), layoutInflater as ViewGroup, false)
+        LayoutFloatingWindowBinding.inflate(
+            LayoutInflater.from(mContext),
+            layoutInflater as ViewGroup,
+            false
+        )
+    }
+
+    private val initParams by lazy {
+        WindowManager.LayoutParams().apply {
+            type = WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY
+            format = PixelFormat.TRANSLUCENT
+            flags = WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
+            gravity = Gravity.END or Gravity.TOP
+            width = WindowManager.LayoutParams.WRAP_CONTENT
+            height = WindowManager.LayoutParams.WRAP_CONTENT
+        }
     }
 
     private val mParams by lazy {
         WindowManager.LayoutParams().apply {
             type = WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY
-            format = PixelFormat.RGBA_8888
-            flags =
-                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE or WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE
-            gravity = Gravity.END or Gravity.TOP
-            x = 0
-            y = 0
+            format = PixelFormat.TRANSLUCENT
+            flags = WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
+            gravity = Gravity.NO_GRAVITY
             width = WindowManager.LayoutParams.WRAP_CONTENT
             height = WindowManager.LayoutParams.WRAP_CONTENT
         }
     }
 
     private var isShow = false
+
+    @SuppressLint("ClickableViewAccessibility")
+    private fun setupTouchAction() {
+        mBinding.root.setOnTouchListener { view, motionEvent ->
+            val xCord = motionEvent.rawX.toInt()
+            val yCord = motionEvent.rawY.toInt()
+            val xCordDestination: Int
+            val yCordDestination: Int
+            val action: Int = motionEvent.action
+
+            if (action == MotionEvent.ACTION_DOWN) {
+                xInitCord = xCord
+                yInitCord = yCord
+                xInitMargin = mParams.x
+                yInitMargin = mParams.y
+            } else if (action == MotionEvent.ACTION_MOVE) {
+                val xDiffMove: Int = xCord - xInitCord
+                val yDiffMove: Int = yCord - yInitCord
+                xCordDestination = xInitMargin + xDiffMove
+                yCordDestination = yInitMargin + yDiffMove
+
+                mParams.x = xCordDestination
+                mParams.y = yCordDestination
+                mWindowManager.updateViewLayout(view, mParams)
+            }
+            true
+        }
+    }
 
     fun onWindowChange(name: String) {
         if (name.isEmpty()) {
@@ -52,7 +100,8 @@ class FloatingWindow(private val mContext: Context) {
         if (isShow) {
             return
         }
-        mWindowManager.addView(mBinding.root, mParams)
+        mWindowManager.addView(mBinding.root, initParams)
+        setupTouchAction()
         isShow = true
         callback?.windowHide(false)
     }
